030d8a0d0583b95bbe81c4e84c589fe599eb9316
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-24 08:59:04 +0100

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const assert = require('assert');
+
+let inputFile = 'input1.txt';
+let outputFile = 'output1.txt';
+if (process.argv.length > 2) {
+  inputFile = 'input' + process.argv[2] + '.txt';
+  outputFile = 'output' + process.argv[2] + '.txt';
+}
+console.log = function (result) {
+  const expected = fs.readFileSync(outputFile).toString().split('\n');
+  assert.deepStrictEqual(result, expected);
+};
+
+const input = fs.readFileSync(inputFile).toString().split('\n');
+
+function ContestResponse() {
+  //implement your code here using input array
+}
+
+ContestResponse();
