0c654754e182b6edbfab530bc17d622d3c761470
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-26 09:40:11 +0100

    add examples

---
+++ 1811-2/script.js
@@ -0,0 +1,19 @@
+/*******
+ * Read input from STDIN
+ * Use: console.log()  to output your result.
+ * Use: console.error() to output debug information into STDERR
+ * ***/
+
+var input = [];
+
+readline_object.on('line', (value) => {
+  //Read input values
+  input.push(value);
+});
+//Call ContestResponse when all inputs are read
+readline_object.on('close', ContestResponse);
+
+function ContestResponse() {
+  //implement your code here using input array
+  console.error(input);
+}
